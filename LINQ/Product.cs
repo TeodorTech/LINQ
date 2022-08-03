9f6668a04327d95bd1959a96550c22cc093daee9
unknown <teodosius77@gmail.com>
2022-08-03 09:37:28 +0300

    Add solution

---
+++ LINQ/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    public class Car
+    {
+        
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public bool isAvailable { get; set; }
+        public string type { get; set; }
+        public override string ToString() => $"{Name},{Price}";
+        
+
+        public static  List<Car> GenerateListOfProducts()
+            {
+            return new List<Car>
+            {
+                new Car{Name="Honda",Price =15000,isAvailable=true ,type ="Sport"},
+                new Car{Name="Mazda",Price =25000,isAvailable=false,type ="Sport"} ,                
+                new Car{Name="Dacia",Price =1000,isAvailable=false,type ="Clasic" } ,
+                new Car{Name="Audi",Price =40000,isAvailable=true,type ="Comfort"},
+                new Car{Name="Porche",Price =50000,isAvailable=true,type ="Sport"},
+                new Car{Name="Mercedes",Price =100500,isAvailable=false,type ="Comfort"},
+                new Car{Name="BMW",Price =50500,isAvailable=true,type ="Sport"},
+
+            };
+
+            }
+}
+}
---
+++ LINQ/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using LINQ;
+
+var listCar = Car.GenerateListOfProducts();
+
+var availableCarsList = listCar.Where(c => c.isAvailable.Equals(true))
+    .OrderByDescending(c => c.Price)
+    .Select(c => c.Name);
+/*foreach (var car in availableCarsList)
+{
+    Console.WriteLine(car);
+
+}*/
+
+var groupedCarsList = listCar.GroupBy(c => c.isAvailable);
+
+foreach (var group in groupedCarsList)
+{
+    Console.WriteLine(group.Key + ":" + group.Count());
+
+    foreach (var car in group)
+    {
+        Console.WriteLine(car);
+    }
+}
+
+var listOfExpensiveCars = listCar.Where(c => c.Price > 40000 && c.type=="Sport"|| c.type=="Comfort").Select(c => new { c.Name ,c.type} );
+/*foreach (var car in listOfExpensiveCars)
+{
+    Console.WriteLine($"{car.Name} is an expensive and {car.type} car");
+}*/
+
+var listOfCheapCars = listCar.Where(c => c.Price <= 30000).Select(c =>new { c.Name, c.type });
+
+/*foreach (var car in listOfCheapCars)
+{
+    Console.WriteLine($"{car.Name} is an affordable and {car.type} car");
+}*/
+
+
